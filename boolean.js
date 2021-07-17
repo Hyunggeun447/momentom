9dab026d2ed5ea8cc9d3ca648b5d50f614af3a6b
Hyunggeun447 <bopi447@naver.com>
2021-07-17 19:49:36 +0900

    study

---
+++ boolean.js
@@ -0,0 +1,13 @@
+//boolean, true or false
+
+const a = null; // 자연적으로 발생 x 개발자의 의도가 반드시 들어감.
+const b = true;
+const c = false;
+const d = undefined;
+let e; // const e; => error, 변수 지정 후 값 변환이 안되기때문.
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
