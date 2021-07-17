f6518cb4e229b4e1589de2c0f4408215c1e975e8
Hyunggeun447 <bopi447@naver.com>
2021-07-17 19:37:59 +0900

    study

---
+++ const and let.js
@@ -0,0 +1,13 @@
+const a = 5;
+const b = "2";     // const cant change variable
+let myName = "Hg"; // let can change variable
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(myName);
+
+
+myName = "Gh";
+console.log(myName);
