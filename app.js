fdc29316e66ea0704d8dba6d9e83e2389c5b2252
Hyunggeun447 <bopi447@naver.com>
2021-07-18 18:42:37 +0900

    no message

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
-const a = 5;
-const b = "2";
-const myName = "Hg";
+//Array
 
-console.log(a+b);
-console.log(a-b);
-console.log(a*b);
-console.log(a/b);
+/*
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
+const sun = "sun";
+*/
 
+
+
+const dayOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+
+const nonsense = [1,2,3,4,5,6,7,8];
+
+
+console.log(dayOfWeek);
+console.log(nonsense);
+console.log(dayOfWeek,nonsense);
+
+console.log(dayOfWeek[2]);
+console.log(dayOfWeek[5]);
+
+dayOfWeek.push("hollyday");  // 마지막 elemunt 추가
+console.log(dayOfWeek);
+
+dayOfWeek.splice(2,2);    // splice(a,b) a부터 b개 제거
+console.log(dayOfWeek);
