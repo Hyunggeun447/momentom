ec74d7f83f46945bbcd21b185c7e3f42b0f02161
Hyunggeun447 <bopi447@naver.com>
2021-07-22 23:16:28 +0900

    no message

---
+++ codeup_1270.py
@@ -0,0 +1,34 @@
+b=input()
+
+n=int(b)
+
+
+
+def how_many(a,n,k):
+  arr=[0]*(n+10)
+  arr[k]=1
+  x=0
+ 
+
+  for i in range(10,n+1):
+    arr[i]=arr[i%10]
+
+  
+  for i in range(a,n+1):
+    x+=arr[i]
+    
+
+  
+
+  
+    
+   
+        
+  return x
+
+
+
+
+
+
+print(how_many(0,n,1))
---
+++ codeup_1407.py
@@ -0,0 +1,4 @@
+arr=input().split()
+
+for i in range (0,len(arr)):
+    print(arr[i],end="")
