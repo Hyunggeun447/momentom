a4f54eb75578036470a1c4afc78d121f62bb2905
Hyunggeun447 <bopi447@naver.com>
2021-07-29 00:06:31 +0900

    no message

--- codeup_1369.py
+++ codeup_1369.py
@@ -1,3 +1,4 @@
+'''
 n,m=map(int, input().split())
 if n>=m:
   k= (n//m)*(m+1)
@@ -33,3 +34,17 @@ else:
     print(' '*(n-2),end="")
     print('*')
   print('*'*n)
+
+  '''
+n,k=map(int, input().split())
+
+for i in range (1,n+1):
+  for j in range(1,n+1):
+    if i==1 or i==n or j==1 or j==n:
+      print('*',end="")
+    elif (i+j-1) % k ==0:
+      print('*',end="")
+    else:
+      print(" ",end="")
+
+  print()
