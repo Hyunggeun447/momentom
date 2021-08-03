16932057806a48dc3a106483a13645427fd1cbf1
Hyunggeun447 <bopi447@naver.com>
2021-08-03 23:08:26 +0900

    no message

--- practice.py
+++ practice.py
@@ -4,7 +4,7 @@ stages=[2, 1, 2, 6, 2, 4, 3, 3]
 
 l=len(stages)
 
-print(l)    #총원
+
 
 dic={}
 for i in range(1,n+2):
@@ -22,7 +22,15 @@ dic_percent={}
 for i in range(1,n+2):
   dic_percent[i]="{:.2f}".format(dic[i]/(l-dic_total[i-1]))
 
+del dic_percent[n+1]
+
+
+dic_percent=sorted(dic_percent.items(), key=lambda x: x[1],reverse=True)
 
 
 
-print(dic_percent)
+answer=[]
+for i in range(0,n):
+  answer.append(dic_percent[i][0])
+
+print(answer)
