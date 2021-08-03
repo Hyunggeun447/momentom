02af63c6792f45e75dc6113e48b6b9011959c2b7
Hyunggeun447 <bopi447@naver.com>
2021-08-03 20:18:42 +0900

    no message

--- programmers_kakao_test_2021.py
+++ programmers_kakao_test_2021.py
@@ -1,3 +1,4 @@
+'''
 def solution(s):
     
     
@@ -17,3 +18,23 @@ def solution(s):
     
     
     return answer
+
+    '''
+    def solution(s):
+    
+    dic={"zero":0,
+         "one":1,
+         "two":2,
+         "three":3,
+         "four":4,
+         "five":5,
+         "six":6,
+         "seven":7,
+         "eight":8,
+         "nine":9,
+        }
+    
+    for item in dic:
+        s=s.replace(item,str(dic.get(item)))
+        
+    return int(s)
