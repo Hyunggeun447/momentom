1edff73546e971595f83c4b1ee56666dd445007a
Hyunggeun447 <bopi447@naver.com>
2021-08-03 23:12:14 +0900

    no message

--- practice.py
+++ practice.py
@@ -1,36 +1,38 @@
+def solution(n, stages):
+    answer = []
+    
+    l=len(stages)
 
-n=5
-stages=[2, 1, 2, 6, 2, 4, 3, 3]	
+    dic={}
+    for i in range(1,n+2):
+        dic[i]=0
 
-l=len(stages)
+    for i in stages:
+        dic[i]+=1
 
+    dic_total={0:0}
+    for i in range(1,n+2):
+        dic_total[i]=dic_total[i-1]+dic[i]
 
+    dic_percent={}
 
-dic={}
-for i in range(1,n+2):
-  dic[i]=0
+    for i in range(1,n+2):
+        dic_percent[i]=dic[i]/(l-dic_total[i-1])
 
-for i in stages:  #각 스테이지 인원
-  dic[i]+=1
-
-dic_total={0:0}
-for i in range(1,n+2):
-  dic_total[i]=dic_total[i-1]+dic[i]
-
-dic_percent={}
-
-for i in range(1,n+2):
-  dic_percent[i]="{:.2f}".format(dic[i]/(l-dic_total[i-1]))
-
-del dic_percent[n+1]
+    del dic_percent[n+1]
 
 
-dic_percent=sorted(dic_percent.items(), key=lambda x: x[1],reverse=True)
+    dic_percent=sorted(dic_percent.items(), key=lambda x: x[1],reverse=True)
 
 
 
-answer=[]
-for i in range(0,n):
-  answer.append(dic_percent[i][0])
-
-print(answer)
+    for i in range(0,n):
+        answer.append(dic_percent[i][0])
+    
+    
+    
+                 
+    
+    
+    
+    return answer
