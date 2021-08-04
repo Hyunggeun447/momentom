4eff34c813e1b1b021f7087e5fd7167be3da3b2d
Hyunggeun447 <bopi447@naver.com>
2021-08-04 22:28:00 +0900

    no message

---
+++ programmers_practice_NumsWithInterval.py
@@ -0,0 +1,10 @@
+def solution(x, n):
+    answer = []
+    
+    if x!=0:
+        for i in range(x,x*(n+1),x):
+            answer.append(i)
+    elif x==0:
+        answer=[0]*n
+    
+    return answer
