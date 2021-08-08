243b776d14400ebc97cd69b1dc19a82033e46eb0
Hyunggeun447 <bopi447@naver.com>
2021-08-08 23:44:32 +0900

    no message

---
+++ programmers_practice_RemoveMinNum.py
@@ -0,0 +1,15 @@
+
+def solution(arr):
+    answer = []
+    
+    if len(arr)==1:
+        answer.append(-1)
+    else:
+        del arr[arr.index(min(arr))]
+        answer=arr
+    
+    
+    
+    
+    
+    return answer
