6d3cc1b1f6b0a9048cd0cdbd92e850650532eaa2
Hyunggeun447 <bopi447@naver.com>
2021-08-09 22:50:23 +0900

    no message

--- programmers_kakaointern.py
+++ programmers_kakaointern.py
@@ -1,6 +1,6 @@
 
 
-
+'''
 def solution(numbers, hand):
     answer = ''
     def distance(a,b):
@@ -90,6 +90,100 @@ def solution(numbers, hand):
                     else:
                         answer+='L'
                         l=k
+                        
+                        
+            
+    
+    return answer
+
+
+    '''
+    
+
+
+def solution(numbers, hand):
+    answer = ''
+    
+    left=[1,4,7]
+    right=[3,6,9]
+    middle=[0,2,5,8]
+    l=10
+    r=11
+    
+    for i in numbers:
+        if i in left:
+            answer+='L'
+            l=i
+        elif i in right:
+            answer+='R'
+            r=i
+        elif i in middle:
+            if i==2:
+                k=[3,1,0,1,2,1,2,3,2,3,4,4]
+                if k[l]<k[r]:
+                    answer+="L"
+                    l=i
+                elif k[l]>k[r]:
+                    answer+="R"
+                    r=i
+                elif k[l]==k[r]:
+                    if hand=="right":
+                        answer+="R"
+                        r=i
+                    else:
+                        answer+="L"
+                        l=i
+                        
+            elif i==5:
+                k=[2,2,1,2,1,0,1,2,1,2,3,3]
+                if k[l]<k[r]:
+                    answer+="L"
+                    l=i
+                elif k[l]>k[r]:
+                    answer+="R"
+                    r=i
+                elif k[l]==k[r]:
+                    if hand=="right":
+                        answer+="R"
+                        r=i
+                    else:
+                        answer+="L"
+                        l=i
+                        
+            elif i==8:
+                k=[1,3,2,3,2,1,2,1,0,1,2,2]
+                if k[l]<k[r]:
+                    answer+="L"
+                    l=i
+                elif k[l]>k[r]:
+                    answer+="R"
+                    r=i
+                elif k[l]==k[r]:
+                    if hand=="right":
+                        answer+="R"
+                        r=i
+                    else:
+                        answer+="L"
+                        l=i
+                
+            elif i==0:
+                k=[0,4,3,4,3,2,3,2,1,2,1,1]
+                if k[l]<k[r]:
+                    answer+="L"
+                    l=i
+                elif k[l]>k[r]:
+                    answer+="R"
+                    r=i
+                elif k[l]==k[r]:
+                    if hand=="right":
+                        answer+="R"
+                        r=i
+                    else:
+                        answer+="L"
+                        l=i
+                    
+            
+
                         
                         
             
