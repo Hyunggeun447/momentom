048f31dd3e61bbb340c278d826ef9b7db24a6eeb
Hyunggeun447 <bopi447@naver.com>
2021-08-11 22:54:46 +0900

    clear

--- programmers_mcc_balloon.py
+++ programmers_mcc_balloon.py
@@ -4,15 +4,18 @@ def solution(a):
     if len(a)>3:
         s=a[1:-1]
         k=a.index(min(s))
+        x=a[0]
 
         for i in range(1,k):
-            if a[i]<a[0]:
+            if a[i]<x:
                 answer+=1
+                x=a[i]
         
-
+        y=a[-1]
         for i in range(len(a)-1,k,-1):
-            if a[i]<a[-1]:
+            if a[i]<y:
                 answer+=1
+                y=a[i]
     else:
         answer=len(a)
     
